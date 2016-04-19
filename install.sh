5303e02438ecb59feb117f3e5bc230730c68fac1
CullenTaylor <mctaylor@us.ibm.com>
2016-04-19 16:12:29 -0500

    sudo pip install

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ reporter "installing apt packages"
 xargs sudo apt-get -y install < data/apt.list
 
 reporter "installing pip packages"
-pip install -r data/pip.list
+sudo pip install -r data/pip.list
 
 reporter "installing chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
